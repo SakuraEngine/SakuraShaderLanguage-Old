70f3e16964262b7e58519b508b8b319489ed4dfa
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2022-11-11 17:59:29 +0800

    移除 App 的 window 目标设置

--- SakuraShaderLanguage/SKSL.Test/UnitTest1.cs
+++ SakuraShaderLanguage/SKSL.Test/UnitTest1.cs
@@ -1,10 +1,19 @@
 namespace SKSL.Test
 {
+    public class ComputeShader : IComputeShader
+    {
+        public void Execute()
+        {
+            var f2 = new float2(1.0f, 2.0f);
+            bool2 b2 = Hlsl.FloatToBool(f2);
+        }
+    }
+
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod()
         {
 
         }
